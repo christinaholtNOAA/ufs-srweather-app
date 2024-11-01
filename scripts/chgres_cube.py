43fedf3a5d9a71eaf1474aad62a6bff0ecd15fb7
WeirAE <weirbs@gmail.com>
2024-11-01 16:48:04 +0000

    Additional changes from comments

--- scripts/chgres_cube.py
+++ scripts/chgres_cube.py
@@ -17,16 +17,17 @@ from uwtools.api.fs import copy as uwcopy
 from uwtools.api.logging import use_uwtools_logger
 
 
-def _deliver_files(config, dst_dir, key_path, src_dir): 
+def _deliver_files(config, dst_dir, key_path, src_dir):
     """
     Deliver files defined in the config "outuput_file_links" section.
     """
     dst_dir.mkdir(exist_ok=True)
     output_links = _walk_key_path(config, key_path + ["output_file_links"])
-    output_links = {k: str(src_dir/v) for k, v in output_links.items()}
+    output_links = {k: str(src_dir / v) for k, v in output_links.items()}
     if not uwcopy(target_dir=dst_dir, config=output_links):
         logging.error("Files could not be copied to their final destination.")
         sys.exit(1)
+
 
 def _walk_key_path(config, key_path):
     """
@@ -93,22 +94,27 @@ def run_chgres_cube(config_file, cycle, key_path, member):
     expt_config = get_yaml_config(config_file)
 
     # The experiment config will have {{ CRES | env }} expressions in it that need to be
-    # dereferenced during driver initialization
+    # dereferenced during driver initialization.
     os.environ["CRES"] = expt_config["workflow"]["CRES"]
     os.environ["MEMBER"] = member
 
     # Render names of external model files in experiment config.
     expt_config.dereference(
         context={
-            "cycle": cycle,
-            **os.environ,
             **expt_config,
+            **os.environ,
+            "cycle": cycle,
         }
     )
-    chgres_cube_config = _walk_key_path(expt_config, key_path)
-    grib2_input = _walk_key_path(chgres_cube_config, ["chgres_cube", "namelist", "update_values", "config"]).get("input_type") == "grib2"
+    task_config = _walk_key_path(expt_config, key_path)
+    grib2_input = (
+        _walk_key_path(
+            task_config, ["chgres_cube", "namelist", "update_values", "config"]
+        ).get("input_type")
+        == "grib2"
+    )
 
-    varsfilepath = chgres_cube_config["input_files_metadata_path"]
+    varsfilepath = task_config["input_files_metadata_path"]
     external_config = get_yaml_config(varsfilepath)
     external_config_fns = external_config["external_model_fns"]
     external_config_fhrs = external_config["external_model_fhrs"]
@@ -131,22 +137,29 @@ def run_chgres_cube(config_file, cycle, key_path, member):
         chgres_cube_driver.run()
 
         if not (rundir / "runscript.chgres_cube.done").is_file():
-            logging.error("Error occurred running chgres_cube. See component error logs.")
+            logging.error(
+                "Error occurred running chgres_cube. See component error logs."
+            )
             sys.exit(1)
 
-        # Deliver output data to the forecast's INPUT dir
+        # Deliver output data to the forecast's INPUT dir.
         delivery_dir = rundir.parent / "INPUT"
-        _deliver_files(config=expt_config, dst_dir=delivery_dir, key_path=key_path, src_dir=rundir)
+        _deliver_files(
+            config=expt_config, dst_dir=delivery_dir, key_path=key_path, src_dir=rundir
+        )
 
-    else: # loop over make_lbcs tasks
-        # This loop will need a version of the config that is not dereferenced
+    else:  # Loop over make_lbcs tasks.
+        # This loop will need a version of the config that is not dereferenced.
         expt_config = get_yaml_config(config_file)
 
         fhrs_and_fns = list(zip(external_config_fhrs, external_config_fns))
         for external_fhr, external_fn in fhrs_and_fns:
             os.environ["fn_grib2" if grib2_input else "fn_atm"] = external_fn
 
-            lbc_offset_fhrs = _walk_key_path(expt_config, ["task_get_extrn_lbcs", "envvars", "EXTRN_MDL_LBCS_OFFSET_HRS"])
+            lbc_offset_fhrs = _walk_key_path(
+                expt_config,
+                ["task_get_extrn_lbcs", "envvars", "EXTRN_MDL_LBCS_OFFSET_HRS"],
+            )
             fcst_hr_lam = int(external_fhr) - int(lbc_offset_fhrs)
             leadtime = dt.timedelta(hours=fcst_hr_lam)
 
@@ -161,26 +174,33 @@ def run_chgres_cube(config_file, cycle, key_path, member):
             chgres_cube_driver.run()
 
             if not (rundir / "runscript.chgres_cube.done").is_file():
-                logging.error("Error occurred running chgres_cube. See component error logs.")
+                logging.error(
+                    "Error occurred running chgres_cube. See component error logs."
+                )
                 sys.exit(1)
 
             # Use a copy of the original here to avoid opening the file every time.
             expt_config_cp = get_yaml_config(deepcopy(expt_config.data))
 
-            # output files should contain cycle information relative to the start time of the
-            # forecast -- use cycle = cycle
-            # This dereferencing must be inside loop bc the fcst hour is set differently each time
+            # Output files should contain cycle information relative to the start time of the
+            # forecast -- use cycle = cycle.
+            # This dereferencing must be inside loop bc the fcst hour is set differently each time.
             expt_config_cp.dereference(
                 context={
+                    **expt_config_cp,
+                    **os.environ,
                     "cycle": cycle,
                     "leadtime": leadtime,
-                    **os.environ,
-                    **expt_config_cp,
                 }
             )
-            # Deliver output data to the forecast's INPUT dir
+            # Deliver output data to the forecast's INPUT dir.
             delivery_dir = rundir.parent / "INPUT"
-            _deliver_files(config=expt_config_cp, dst_dir=delivery_dir, key_path=key_path, src_dir=rundir)
+            _deliver_files(
+                config=expt_config_cp,
+                dst_dir=delivery_dir,
+                key_path=key_path,
+                src_dir=rundir,
+            )
 
 
 if __name__ == "__main__":
